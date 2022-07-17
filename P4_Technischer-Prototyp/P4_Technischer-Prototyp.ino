e69e09b83083188e520e0f74203899ac5fdb8737
stjesnay <jennifer.snay@stud.h-da.de>
2022-07-17 13:17:39 +0200

    Update P4_Technischer-Prototyp.ino

--- P4_Technischer-Prototyp/P4_Technischer-Prototyp.ino
+++ P4_Technischer-Prototyp/P4_Technischer-Prototyp.ino
@@ -12,10 +12,10 @@ void loop() {
   licht = analogRead(0); //ließt Sensorwert aus und speichert ihn in "licht"
   Serial.println(licht); 
 
-  if(licht<400){ //wenn der Wert unter 400 ist, soll die LED leuchten
+  if(licht<200){ //wenn der Wert unter 200 ist, soll die LED leuchten
     digitalWrite(13, HIGH);
     
-  } else { //wenn der Wert größer oder gleich 400 ist, soll die LED aus bleiben
+  } else { //wenn der Wert größer oder gleich 200 ist, soll die LED aus bleiben
     digitalWrite(13,LOW);
   }
 }
